0831b0fedbe8bdb5959298377d19499f06c5db42
Ernest <asaern9@gmail.com>
2019-07-23 00:15:02 +0000

    ed_1

--- blog/views.py
+++ blog/views.py
@@ -11,3 +11,39 @@ def index(request):
 class NewsDetail(DetailView):
     model = News
     template_name = 'blog/news_detail.html'
+
+
+def arts_culture(request):
+    return render(request, 'blog/arts_culture.html')
+
+
+def business(request):
+    return render(request, 'blog/business.html')
+
+
+def entertainment(request):
+    return render(request, 'blog/entertainment.html')
+
+
+def fashion(request):
+    return render(request, 'blog/fashion.html')
+
+
+def health(request):
+    return render(request, 'blog/health.html')
+
+
+def politics(request):
+    return render(request, 'blog/politics.html')
+
+
+def sport(request):
+    return render(request, 'blog/sport.html')
+
+
+def tech(request):
+    return render(request, 'blog/tech.html')
+
+
+def contact(request):
+    return render(request, 'blog/contact.html')
