da32848f9193d77a971ae951781b0920bf7691f7
Ernest <asaern9@gmail.com>
2019-07-25 21:50:58 +0000

    ed_2

--- blog/urls.py
+++ blog/urls.py
@@ -9,28 +9,12 @@ urlpatterns = [
     path('news/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
 
     path('arts_culture/', views.arts_culture, name='blog-arts-culture'),
-    path('arts_culture/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('business/', views.business, name='blog-business'),
-    path('business/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('entertainment/', views.entertainment, name='blog-entertainment'),
-    path('entertainment/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('fashion/', views.fashion, name='blog-fashion'),
-    path('fashion/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('health/', views.health, name='blog-health'),
-    path('health/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('politics/', views.politics, name='blog-politics'),
-    path('politics/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('sports/', views.sport, name='blog-sport'),
-    path('sports/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('technology/', views.tech, name='blog-technology'),
-    path('technology/<slug>/', NewsDetail.as_view(), name='blog-news-detail'),
-
     path('contact/', views.contact, name='blog-contact'),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- blog/views.py
+++ blog/views.py
@@ -1,6 +1,6 @@
 from django.shortcuts import render, reverse
 from django.views.generic import DetailView
-from django.core.paginator import Paginator
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
 from .models import News
 # Create your views here.
 
@@ -16,36 +16,122 @@ class NewsDetail(DetailView):
     def get_context_data(self, *args, **kwargs):
         context = super(NewsDetail, self).get_context_data(*args, **kwargs)
         context['related_post'] = News.objects.filter(category='politics').order_by('-id')[:4]
+        context['related_post_arts'] = News.objects.filter(category='arts').order_by('-id')[:4]
         return context
 
 
 def arts_culture(request):
-    arts = News.objects.all().filter(category='arts')
-    context = {'arts': arts}
+    arts = News.objects.all().filter(category='arts').order_by('-id')
+    bus = News.objects.all().filter(category='business').order_by('-id')[:1]
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')[:1]
+    fash = News.objects.all().filter(category='fashion').order_by('-id')[:1]
+    heal = News.objects.all().filter(category='health').order_by('-id')[:1]
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
+    technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(arts, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'business': bus, 'entertainment': enter, 'fashion': fash, 'health': heal,
+               'sports': sports, 'technology': technology, 'news': news}
     return render(request, 'blog/arts_culture.html', context)
 
 
 def business(request):
-    bus = News.objects.all().filter(category='business')
-    context = {'business': bus}
+    arts = News.objects.all().filter(category='arts').order_by('-id')[:1]
+    bus = News.objects.all().filter(category='business').order_by('-id')
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')[:1]
+    fash = News.objects.all().filter(category='fashion').order_by('-id')[:1]
+    heal = News.objects.all().filter(category='health').order_by('-id')[:1]
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
+    technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(bus, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'entertainment': enter, 'fashion': fash, 'health': heal,
+               'sports': sports, 'technology': technology, 'news': news}
     return render(request, 'blog/business.html', context)
 
 
 def entertainment(request):
-    enter = News.objects.all().filter(category='entertainment')
-    context = {'entertainment': enter}
+    arts = News.objects.all().filter(category='arts').order_by('-id')[:1]
+    bus = News.objects.all().filter(category='business').order_by('-id')[:1]
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')
+    fash = News.objects.all().filter(category='fashion').order_by('-id')[:1]
+    heal = News.objects.all().filter(category='health').order_by('-id')[:1]
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
+    technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(enter, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'business': bus, 'fashion': fash, 'health': heal,
+               'sports': sports, 'technology': technology, 'news': news}
     return render(request, 'blog/entertainment.html', context)
 
 
 def fashion(request):
-    fash = News.objects.all().filter(category='fashion')
-    context = {'fashion': fash}
+    arts = News.objects.all().filter(category='arts').order_by('-id')[:1]
+    bus = News.objects.all().filter(category='business').order_by('-id')[:1]
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')[:1]
+    fash = News.objects.all().filter(category='fashion').order_by('-id')
+    heal = News.objects.all().filter(category='health').order_by('-id')[:1]
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
+    technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(fash, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'business': bus, 'entertainment': enter, 'health': heal,
+               'sports': sports, 'technology': technology, 'news': news}
     return render(request, 'blog/fashion.html', context)
 
 
 def health(request):
-    heal = News.objects.all().filter(category='health')
-    context = {'health': heal}
+    arts = News.objects.all().filter(category='arts').order_by('-id')[:1]
+    bus = News.objects.all().filter(category='business').order_by('-id')[:1]
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')[:1]
+    fash = News.objects.all().filter(category='fashion').order_by('-id')[:1]
+    heal = News.objects.all().filter(category='health').order_by('-id')
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
+    technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(heal, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'business': bus, 'entertainment': enter, 'fashion': fash,
+               'sports': sports, 'technology': technology, 'news': news}
     return render(request, 'blog/health.html', context)
 
 
@@ -59,20 +145,62 @@ def politics(request):
     sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
     technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
 
+    page = request.GET.get('page', 1)
     paginator = Paginator(poli, 10)
-    context = {'politics': poli, 'arts': arts, 'business': bus, 'entertainment': enter, 'fashion': fash, 'health': heal, 'sports': sports, 'technology': technology}
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'business': bus, 'entertainment': enter, 'fashion': fash, 'health': heal,
+               'sports': sports, 'technology': technology, 'news': news}
     return render(request, 'blog/politics.html', context)
 
 
 def sport(request):
-    sports = News.objects.all().filter(category='sport')
-    context = {'sports': sports}
+    arts = News.objects.all().filter(category='arts').order_by('-id')[:1]
+    bus = News.objects.all().filter(category='business').order_by('-id')[:1]
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')[:1]
+    fash = News.objects.all().filter(category='fashion').order_by('-id')[:1]
+    heal = News.objects.all().filter(category='health').order_by('-id')[:1]
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')
+    technology = News.objects.all().filter(category='tech').order_by('-id')[:1]
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(sports, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'business': bus, 'entertainment': enter, 'fashion': fash, 'health': heal,
+               'technology': technology, 'news': news}
     return render(request, 'blog/sport.html', context)
 
 
 def tech(request):
-    technology = News.objects.all().filter(category='tech')
-    context = {'arts': technology}
+    arts = News.objects.all().filter(category='arts').order_by('-id')[:1]
+    bus = News.objects.all().filter(category='business').order_by('-id')[:1]
+    enter = News.objects.all().filter(category='entertainment').order_by('-id')[:1]
+    fash = News.objects.all().filter(category='fashion').order_by('-id')[:1]
+    heal = News.objects.all().filter(category='health').order_by('-id')[:1]
+    poli = News.objects.all().filter(category='politics').order_by('-id')[:1]
+    sports = News.objects.all().filter(category='sport').order_by('-id')[:1]
+    technology = News.objects.all().filter(category='tech').order_by('-id')
+
+    page = request.GET.get('page', 1)
+    paginator = Paginator(technology, 10)
+    try:
+        news = paginator.page(page)
+    except PageNotAnInteger:
+        news = paginator.page(1)
+    except EmptyPage:
+        news = paginator.page(paginator.num_pages)
+    context = {'politics': poli, 'arts': arts, 'business': bus, 'entertainment': enter, 'fashion': fash, 'health': heal,
+               'sports': sports, 'news': news}
     return render(request, 'blog/tech.html', context)
 
 
